6c25ab8e4c80e3934e8bf26b151cac93e0a4862c
NOLi7000 <majin.dmx00000@gmail.com>
2025-10-30 21:31:58 +0300

    first commit

--- Pizzarestaurant.java
+++ Pizzarestaurant.java
@@ -27,10 +27,12 @@ public class Pizzarestaurant {
         Pizza order1 = new Pizza("Ali", "Pepperoni");
         Pizza order2 = new Pizza("Khalid", "Margherita");
         Pizza order3 = new Pizza("Jabbir", "BBQ Chicken");
+        Pizza order4 = new Pizza("Jabbir", "BBQ Chicken");
 
         System.out.println("Order ID: " + order1.getOrderId() + ", Customer: " + order1.getCustomerName() + ", Pizza: " + order1.getPizzaType());
         System.out.println("Order ID: " + order2.getOrderId() + ", Customer: " + order2.getCustomerName() + ", Pizza: " + order2.getPizzaType());
         System.out.println("Order ID: " + order3.getOrderId() + ", Customer: " + order3.getCustomerName() + ", Pizza: " + order3.getPizzaType());
+        System.out.println("Order ID: " + order4.getOrderId() + ", Customer: " + order4.getCustomerName() + ", Pizza: " + order4.getPizzaType());
 
     }
 }
