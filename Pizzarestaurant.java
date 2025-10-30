2e80fa6fb296053ecf0b8913da9ee30a53620dad
NOLi7000 <majin.dmx00000@gmail.com>
2025-10-30 20:10:32 +0300

    first commit

---
+++ Pizzarestaurant.java
@@ -0,0 +1,40 @@
+class Pizza{
+
+     private static int orderCounter = 0;
+    private int orderId;         
+    private String customerName;  
+    private String pizzaType;     
+    {
+        orderId = ++orderCounter; 
+    }
+    Pizza(String customerName, String pizzaType) {
+        this.customerName = customerName;
+        this.pizzaType = pizzaType;
+    }
+    public int getOrderId() {
+        return orderId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getPizzaType() {
+        return pizzaType;
+    }
+   
+}
+public class Pizzarestaurant {
+   
+   
+    
+    public static void main(String[] args) {
+        
+        Pizza order1 = new Pizza("Ali", "Pepperoni");
+        Pizza order2 = new Pizza("Khalid", "Margherita");
+        Pizza order3 = new Pizza("Jabir", "BBQ Chicken");
+
+        System.out.println("Order ID: " + order1.getOrderId() + ", Customer: " + order1.getCustomerName() + ", Pizza: " + order1.getPizzaType());
+        System.out.println("Order ID: " + order2.getOrderId() + ", Customer: " + order2.getCustomerName() + ", Pizza: " + order2.getPizzaType());
+        System.out.println("Order ID: " + order3.getOrderId() + ", Customer: " + order3.getCustomerName() + ", Pizza: " + order3.getPizzaType());
+
+    }
+}
